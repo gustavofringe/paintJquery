52795a3810bbdbc2062c7d50f1a4b93afe39844c
Guillaume <dussartguillaume@orange.fr>
2017-07-22 13:32:45 +0200

    main.js->add function paint

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/**
+ *
+ * variable
+ *
+ * */
+var canvas = document.getElementById('draw');
+var ctx = canvas.getContext('2d');
+var click;
+
+canvas.width = $('#draw').width();
+canvas.height= $('#draw').height();
+/**
+ *
+ * active action mouse on press click
+ *
+ * */
+$('#draw').mousedown(function (event) {
+    click = true;
+    ctx.moveTo(event.pageX, event.pageY);
+});
+/**
+ *
+ * move action
+ *
+ * */
+$('#draw').mousemove(function (event) {
+    if (click) {
+        ctx.beginPath();
+        ctx.fillStyle = $('input[type=color]').val();
+        ctx.arc(event.offsetX, event.offsetY, $('input[type=range]').val(), 0, 360);
+        ctx.fill();
+    }
+});
+/**
+ *
+ *delete action mouse
+ *
+ * */
+$('#draw').mouseup(function () {
+    click = false;
+});
+/**
+ *
+ * reset draw
+ *
+ *
+ * */
+$('#reset').click(function(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
